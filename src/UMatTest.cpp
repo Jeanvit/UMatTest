bbc5d13e78b44d2eeccc88cfca67f7e3f57e7c96
Jean <jeanvitordepaulo@gmail.com>
2018-02-15 17:58:45 -0200

    Adding Readme and Comments

--- src/UMatTest.cpp
+++ src/UMatTest.cpp
@@ -20,15 +20,40 @@ using std::cout;
 using std::endl;
 /*****************************************************************************************************************************/
 //Headers
+
+
+/**
+    Process a given Mat image, converting it to CV_BGR2GRAY
+    and then apply a bilateral filter
+
+    @param const std::string& name The name of the image.
+*/
+
 void MatProcessing(const std::string& name);
+
+/**
+    Process a given UMat image, converting it to CV_BGR2GRAY
+    and then apply a bilateral filter
+
+    @param const std::string& name The name of the image.
+*/
+
 void UMatProcessing(const std::string& name);
+
+/**
+    Verify using a std::ifstream if a file exists
+
+    @param const std::string& name The name of file
+    @return a bool, true if the file exists, false otherwise
+*/
 bool fileExists(std::string& filename);
 /*****************************************************************************************************************************/
+
 //Main
 auto main(int argc, char *argv[]) -> int {
 	std::string imageName;
-	high_resolution_clock::time_point start,end;
-	duration<double> elapsedTime;
+	high_resolution_clock::time_point start,end; // for time measurement
+	duration<double> elapsedTime;			 	 // for time measurement
 	int type;
 	cout << "OpenCL is available? " << cv::ocl::haveOpenCL() << endl; // returns true, OCL available
 	ocl::setUseOpenCL(true);
@@ -70,6 +95,7 @@ auto main(int argc, char *argv[]) -> int {
 	return EXIT_SUCCESS;
 }
 /*****************************************************************************************************************************/
+// Process a given Mat image, converting it to CV_BGR2GRAY and then apply a bilateral filter
 void MatProcessing(const std::string& name){
 	Mat img,dst;
 	img = imread(name.c_str());
@@ -77,13 +103,15 @@ void MatProcessing(const std::string& name){
 	bilateralFilter(img, dst, 0, 10, 3);
 }
 /*****************************************************************************************************************************/
+//Process a given UMat image, converting it to CV_BGR2GRAY and then apply a bilateral filter
 void UMatProcessing(const std::string& name){
 	UMat Uimg, Udst;
-	Uimg = imread(name.c_str(), IMREAD_COLOR).getUMat(ACCESS_READ);
+	Uimg = imread(name.c_str(), IMREAD_UNCHANGED).getUMat(ACCESS_READ);
 	cv::cvtColor(Uimg,Uimg,CV_BGR2GRAY);
 	bilateralFilter(Uimg, Udst, 0, 10, 3);
 }
 /****************************************************************************************************************************/
+//Verify using a std::ifstream if a file exists
 bool fileExists(std::string& filename) {
   std::ifstream ifile(filename.c_str());
   return static_cast<bool>(ifile);
